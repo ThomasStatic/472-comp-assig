ed0ba2b48f62bad10b292e05b3c37ac20117ea42
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2026-03-12 12:41:00 -0400

    adding policy extraction to value iteration function

--- Problem 1/a.py
+++ Problem 1/a.py
@@ -20,25 +20,31 @@ def kernel(x0: str, x1: str, u: int) -> float:
             return 0.8 if u == 1 else 0.2
 
 
-def value_iteration(eta) -> dict[str, float]:
+def value_iteration(eta) -> tuple[dict[str, float], dict[str, int]]:
     v_0: dict[str, float] = {'G': 0, 'B': 0}
     v_1: dict[str, float] = {'G': 0, 'B' : 0}
+    policy: dict[str, int] = {'G': 0, 'B': 0}
     iteration: int = 0
     while(True):
         iteration += 1
         for x in Xcal:
-            # we need the minimum of the two actions, so I default set to the first one
+            # we need the minimum of the two actions, so I can just default set it to the first one
             v_1[x] = calc_cost(x, 0, eta) + beta*(kernel(x, 'G', 0)*v_0['G'] + kernel(x, 'B', 0)*v_0['B'])
             
-            # and then we can switch to the second one of it's lower
-            v_1[x] = min(v_1[x], calc_cost(x, 1, eta) + beta*(kernel(x, 'G', 1)*v_0['G'] + kernel(x, 'B', 1)*v_0['B']))
-
+            
+            better_cost = min(v_1[x], calc_cost(x, 1, eta) + beta*(kernel(x, 'G', 1)*v_0['G'] + kernel(x, 'B', 1)*v_0['B']))
+            policy[x] = 0 if better_cost == v_1[x] else 1
+            v_1[x] = better_cost
+            
         if max(abs(v_1[x]-v_0[x]) for x in Xcal) < 1e-6:
-            return v_1
+            print(f"Converged after {iteration} iterations")
+            return v_1, policy
         v_0 = v_1.copy()
 
 if __name__ == "__main__":
     print("Value Iteration")
     for eta in etas:
         print(f"eta: {eta}")
-        print(value_iteration(eta))
+        v1, policy = value_iteration(eta)
+        print(f"Value Function: {v1}")
+        print(f"Policy: {policy}")

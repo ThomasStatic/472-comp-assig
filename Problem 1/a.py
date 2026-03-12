297b2142e51b608b4ba6dff08a29a4fd11c088c6
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2026-03-12 11:27:27 -0400

    refactoring + adding consts

--- Problem 1/a.py
+++ Problem 1/a.py
@@ -1,5 +1,7 @@
-def calc_cost(x, u, eta):
-    if x == 'G':
-        return u(eta-1)
-    return u(eta)
+Xcal = {'B', 'G'}
+Ucal = {0, 1}
+beta = 0.5
+M = 1
 
+def calc_cost(x, u, eta):
+    return u(eta-M) if x == 'G' else u(eta)

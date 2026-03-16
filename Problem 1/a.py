160421feb936c1cd2608b292f7110fd09f39a9ff
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2026-03-16 13:10:04 -0400

    implementing policy iteration

--- Problem 1/a.py
+++ Problem 1/a.py
@@ -41,10 +41,49 @@ def value_iteration(eta) -> tuple[dict[str, float], dict[str, int]]:
             return v_1, policy
         v_0 = v_1.copy()
 
+def policy_iteration(eta) -> tuple[dict[str, float], dict[str, int]]:
+    policy_old: dict[str, int] = {'G': 0, 'B': 0}
+    policy_new: dict[str, int] = {'G': 0, 'B': 0}
+    v: dict[str, float] = {'G': 0, 'B': 0}
+    v_old: dict[str, float] = {'G': 0, 'B': 0}
+    iteration: int = 0
+    while(True):
+        iteration += 1
+
+        while (True):
+            for x in Xcal:
+                v[x] = calc_cost(x, policy_old[x], eta) + beta*compute_future_value(x, policy_old[x], v_old)
+            if max(abs(v[x]-v_old[x]) for x in Xcal) < 1e-6:
+                break
+            v_old = v.copy()
+        
+        for x in Xcal:
+            policy_new[x] = 0 if calc_cost(x, 0, eta) + beta*compute_future_value(x, 0, v) < calc_cost(x, 1, eta) + beta*compute_future_value(x, 1, v) else 1
+        if policy_new == policy_old:
+            break
+        policy_old = policy_new.copy()
+    
+    print(f"Converged after {iteration} iterations")
+    return v, policy_new
+        
+
+def compute_future_value(x0: str, u0: int, v: dict[str, float]) -> float:
+    next_state_value = 0
+    for x1 in Xcal:
+        next_state_value += kernel(x0, x1, u0)*v[x1]
+    return next_state_value
+
+
 if __name__ == "__main__":
-    print("Value Iteration")
     for eta in etas:
+        print("\nValue Iteration")
         print(f"eta: {eta}")
-        v1, policy = value_iteration(eta)
-        print(f"Value Function: {v1}")
+        v, policy = value_iteration(eta)
+        print(f"Value Function: {v}")
+        print(f"Policy: {policy}")
+
+        print("\nPolicy Iteration")
+        print(f"eta: {eta}")
+        v, policy = policy_iteration(eta)
+        print(f"Value Function: {v}")
         print(f"Policy: {policy}")
